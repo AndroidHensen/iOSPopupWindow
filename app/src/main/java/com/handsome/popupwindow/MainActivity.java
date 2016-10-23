7375fc0dbbc195328edd723e6beae33985e73934
mzsjlwl <2855240636@qq.com>
2016-10-23 10:41:53 +0800

    Hensen_

---
+++ app/src/main/java/com/handsome/popupwindow/MainActivity.java
@@ -0,0 +1,103 @@
+package com.handsome.popupwindow;
+
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, PopupWindow.OnDismissListener {
+
+    private Button bt_open;
+    private PopupWindow popupWindow;
+    private int navigationHeight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt_open = (Button) findViewById(R.id.bt_open);
+        bt_open.setOnClickListener(this);
+
+        int resourceId = getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        navigationHeight = getResources().getDimensionPixelSize(resourceId);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_open:
+                openPopupWindow(v);
+                break;
+            case R.id.tv_pick_phone:
+                Toast.makeText(this, "从手机相册选择", Toast.LENGTH_SHORT).show();
+                popupWindow.dismiss();
+                break;
+            case R.id.tv_pick_zone:
+                Toast.makeText(this, "从空间相册选择", Toast.LENGTH_SHORT).show();
+                popupWindow.dismiss();
+                break;
+            case R.id.tv_cancel:
+                popupWindow.dismiss();
+                break;
+        }
+    }
+
+    private void openPopupWindow(View v) {
+        //防止重复按按钮
+        if (popupWindow != null && popupWindow.isShowing()) {
+            return;
+        }
+        //设置PopupWindow的View
+        View view = LayoutInflater.from(this).inflate(R.layout.view_popupwindow, null);
+        popupWindow = new PopupWindow(view, RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        //设置背景,这个没什么效果，不添加会报错
+        popupWindow.setBackgroundDrawable(new BitmapDrawable());
+        //设置点击弹窗外隐藏自身
+        popupWindow.setFocusable(true);
+        popupWindow.setOutsideTouchable(true);
+        //设置动画
+        popupWindow.setAnimationStyle(R.style.PopupWindow);
+        //设置位置
+        popupWindow.showAtLocation(v, Gravity.BOTTOM, 0, navigationHeight);
+        //设置消失监听
+        popupWindow.setOnDismissListener(this);
+        //设置PopupWindow的View点击事件
+        setOnPopupViewClick(view);
+        //设置背景色
+        setBackgroundAlpha(0.5f);
+    }
+
+    private void setOnPopupViewClick(View view) {
+        TextView tv_pick_phone, tv_pick_zone, tv_cancel;
+        tv_pick_phone = (TextView) view.findViewById(R.id.tv_pick_phone);
+        tv_pick_zone = (TextView) view.findViewById(R.id.tv_pick_zone);
+        tv_cancel = (TextView) view.findViewById(R.id.tv_cancel);
+        tv_pick_phone.setOnClickListener(this);
+        tv_pick_zone.setOnClickListener(this);
+        tv_cancel.setOnClickListener(this);
+    }
+
+    //设置屏幕背景透明效果
+    public void setBackgroundAlpha(float alpha) {
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.alpha = alpha;
+        getWindow().setAttributes(lp);
+    }
+
+    @Override
+    public void onDismiss() {
+        setBackgroundAlpha(1);
+    }
+}
